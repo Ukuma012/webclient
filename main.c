6cd384d32bebb0eaeca9c7b295225517c85a9f0b
Ukuma012 <Ukuma012@gmail.com>
2023-05-13 18:23:38 +0900

    server address

--- main.c
+++ main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
+// struct in_addr
+// {
+//         ip_addr_t s_addr;
+// };
+
+//  struct sockaddr_in {
+//      unsigned char  sin_len;
+//      unsigned char  sin_family;
+//      unsigned short sin_port;
+//      struct in_addr sin_addr;
+//      unsigned char  sin_zero[8];
+// };
+
+int main(int argc, char *argv[])
+{
   int socketfd;
-  if((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+  if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
     fprintf(stderr, "socket failed\n");
     exit(1);
   }
+
+  struct sockaddr_in serveraddress;
+  serveraddress.sin_family = AF_INET;
+  serveraddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serveraddress.sin_port = htons(8000);
 
   printf("%d\n", socketfd);
   exit(0);
